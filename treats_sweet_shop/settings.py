a3b2703f76bffeebfeda688effcc0ea6ea70dfd0
kpsdev1 <stuart151@hotmail.com>
2023-05-22 18:51:02 +0000

    updated profiles app

--- treats_sweet_shop/settings.py
+++ treats_sweet_shop/settings.py
@@ -117,6 +117,7 @@ ACCOUNT_SIGNUP_EMAIL_ENTER_TWICE = True
 ACCOUNT_USERNAME_MIN_LENGTH = 4
 LOGIN_URL = '/accounts/login/'
 LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/accounts/login/'
 
 WSGI_APPLICATION = 'treats_sweet_shop.wsgi.application'
 
@@ -182,6 +183,7 @@ STRIPE_CURRENCY = 'usd'
 STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
 STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
 STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
+DEFAULT_FROM_EMAIL = 'treatssweetshop@example.com'
 
 
 # Default primary key field type

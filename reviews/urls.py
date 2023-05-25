5b724b7b479f1a7578c2fe4867f3ea2074e9954a
kpsdev1 <stuart151@hotmail.com>
2023-05-25 16:59:32 +0000

    styled review section and made it responsive

--- reviews/urls.py
+++ reviews/urls.py
@@ -4,6 +4,10 @@ from .views import *
 
 
 urlpatterns = [
+    path(
+        '<int:pk>/delete_review/', ReviewDeleteView.as_view(), name='delete_review'
+        ),
+    path('<int:pk>/edit_review/', ReviewUpdateView.as_view(), name='edit_review'),
     path('add_review/', ReviewCreateView.as_view(), name='add_review'),
     path('', views.ReviewsList.as_view(), name='reviews'),
 ]
--- reviews/views.py
+++ reviews/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from django.urls import reverse_lazy
+from django.contrib import messages
 from django.views.generic import ListView, DetailView
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.messages.views import SuccessMessageMixin
 from .models import Review
 from .forms import ReviewForm
 # Create your views here.
@@ -17,16 +19,37 @@ class ReviewsList(ListView):
     context_object_name = 'reviews_list'
 
 
-class ReviewCreateView(LoginRequiredMixin, CreateView):
+class ReviewCreateView(LoginRequiredMixin, SuccessMessageMixin, CreateView):
     """
     View to Add a Review
     """
     model = Review
     template_name = 'reviews/add_review.html'
     form_class = ReviewForm
+    success_message = 'Review added successfully'
 
     success_url = reverse_lazy('reviews')
 
     def form_valid(self, form):
         form.instance.posted_by = self.request.user
         return super().form_valid(form)
+
+
+class ReviewUpdateView(LoginRequiredMixin, SuccessMessageMixin, UpdateView):
+    """
+    This is the view to edit a review
+    """
+    model = Review
+    template_name = 'reviews/edit_review.html'
+    form_class = ReviewForm
+    success_message = 'Review updated successfully'
+
+
+class ReviewDeleteView(LoginRequiredMixin, SuccessMessageMixin, DeleteView):
+    """
+    This is the view to delete a review
+    """
+    model = Review
+    success_url = reverse_lazy('reviews')
+    template_name = 'reviews/reviews.html'
+    success_message = 'Review has been deleted'

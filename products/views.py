74d431c9d8e2b291d1fc076b51e8f0f079f07b55
kpsdev1 <stuart151@hotmail.com>
2023-06-13 15:20:28 +0000

    fixing pyhon linter errors for products and home apps

--- products/views.py
+++ products/views.py
@@ -8,8 +8,9 @@ from .forms import ProductForm
 
 # Create your views here.
 
+
 def all_products(request):
-    
+
     products = Product.objects.all()
     query = None
     categories = None
@@ -41,10 +42,11 @@ def all_products(request):
             if not query:
                 messages.error(request, "You didn't enter any search terms")
                 return redirect(reverse('products'))
-            
-            queries = Q(name__icontains=query) | Q(description__icontains=query)
+
+            queries = (Q(name__icontains=query) |
+                       Q(description__icontains=query))
             products = products.filter(queries)
-    
+
     current_sorting = f'{sort}_{direction}'
 
     context = {
@@ -84,10 +86,11 @@ def add_product(request):
             messages.success(request, 'Successfully added product!')
             return redirect(reverse('add_product'))
         else:
-            messages.error(request, 'Failed to add product. Please ensure the form is valid.')
+            messages.error(request, 'Failed to add product. \
+                 Please ensure the form is valid.')
     else:
         form = ProductForm()
-        
+
     template = 'products/add_product.html'
     context = {
         'form': form,
@@ -111,7 +114,8 @@ def edit_product(request, product_id):
             messages.success(request, 'Successfully updated product!')
             return redirect(reverse('product_detail', args=[product.id]))
         else:
-            messages.error(request, 'Failed to update product. Please ensure the form is valid.')
+            messages.error(request, 'Failed to update product. \
+                 Please ensure form is valid.')
     else:
         form = ProductForm(instance=product)
         messages.info(request, f'You are editing {product.name}')
